26ec4d1ff3579acd8cd7686ecd2f22e4e48a747b
Pierre Massey <pierremassey1@gmail.com>
2021-07-20 08:55:31 +0200

    [feat] batch normalization [feat] network initialization

--- model/actor_network.py
+++ model/actor_network.py
@@ -1,37 +1,63 @@
+import numpy as np
+
 import torch
 from torch import nn
 import torch.nn.functional as F
 
 from ccontrol.utils import OptimizerCreator
 
-#TODO : Docstring
 #TODO : Modularize the size of the network
 #TODO : Modularize the last activation function
 #TODO : Add tests
 
 class Actor_network(nn.Module):
-    """[summary]
-    TODO
-    Args:
-        nn ([type]): [description]
+    """
+    function approximation that will directly learn the policy.
+    Using the critic, this network will learn a deteministic mapping
+    between the states and the actions.
     """
     
     def __init__(self, context, config):
         super(Actor_network, self).__init__()
         
+        self.config = config
         self.seed = torch.manual_seed(config.seed)
 
-        self.fc_from_state = nn.Linear(context.state_size, config.fc_hl[0])
-        self.fc_hl_1 = nn.Linear(config.fc_hl[0], config.fc_hl[1])
-        self.fc_to_action = nn.Linear(config.fc_hl[1], context.action_size)
+        self.fc1 = nn.Linear(context.state_size, config.fc_hl[0])
+        self.bn1 = nn.LayerNorm(config.fc_hl[0])
+        self.fc2 = nn.Linear(config.fc_hl[0], config.fc_hl[1])
+        self.bn2 = nn.LayerNorm(config.fc_hl[1])
+        self.fc_to_actions = nn.Linear(config.fc_hl[1], context.action_size)
+        self.initialize_parameters()
 
         self.optimizer = OptimizerCreator().create(
             config.optimizer, self.parameters(), config.optim_kwargs)
 
+    def initialize_parameters(self):
+        '''
+        Initialize the weight and biais of all layers with a unifom distribution 
+        of spread 1 / sqrt(layer_size)
+        '''
+        spread = 1 / np.sqrt(self.fc1.weight.data.size()[0])
+        torch.nn.init.uniform_(self.fc1.weight.data, -spread, spread)
+        torch.nn.init.uniform_(self.fc1.bias.data, -spread, spread)
+
+        spread = 1 / np.sqrt(self.fc2.weight.data.size()[0])
+        torch.nn.init.uniform_(self.fc2.weight.data, -spread, spread)
+        torch.nn.init.uniform_(self.fc2.bias.data, -spread, spread)
+
+        spread = self.config.last_layer_init
+        torch.nn.init.uniform_(self.fc_to_actions.weight.data, -spread, spread)
+        torch.nn.init.uniform_(self.fc_to_actions.bias.data, -spread, spread)
+
     def forward(self, state):
 
-        features = F.relu(self.fc_from_state(state))
-        features = F.relu(self.fc_hl_1(features))
-        actions = torch.tanh(self.fc_to_action(features)) 
+        features = self.bn1(self.fc1(state))
+        features = F.relu(features)
+
+        features = self.bn2(self.fc2(features))
+        features = F.relu(features)
+        
+        actions = torch.tanh(self.fc_to_actions(features)) 
 
         return actions 
--- model/critic_network.py
+++ model/critic_network.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import torch
 from torch import  nn
 import torch.nn.functional as F
@@ -17,22 +19,45 @@ class Critic_network(nn.Module):
         super(Critic_network, self).__init__()
 
         self.seed = torch.manual_seed(config.seed)
-        self.optmizer = config.optimizer
-        self.criterion = config.criterion
+        self.config = config
 
-        self.fc_from_state = nn.Linear(context.state_size, config.fc_hl[0])
-        self.fc_state_action = nn.Linear(config.fc_hl[0] + context.action_size,
-                                         config.fc_hl[1])
+        self.fc1 = nn.Linear(context.state_size, config.fc_hl[0])
+        self.bn1 = nn.LayerNorm(config.fc_hl[0])
+        self.fc2 = nn.Linear(config.fc_hl[0] + context.action_size, config.fc_hl[1])
+        self.bn2 = nn.LayerNorm(config.fc_hl[1])
         self.fc_to_Q = nn.Linear(config.fc_hl[1], 1)
+        self.initialize_parameters()
 
+        self.criterion = config.criterion
         self.optimizer = OptimizerCreator().create(
             config.optimizer, self.parameters(), config.optim_kwargs)
+    
+    def initialize_parameters(self):
+        '''
+        Initialize the weight and biais of all layers with a unifom distribution 
+        of spread 1 / sqrt(layer_size)
+        '''
+        spread = 1 / np.sqrt(self.fc1.weight.data.size()[0])
+        torch.nn.init.uniform_(self.fc1.weight.data, -spread, spread)
+        torch.nn.init.uniform_(self.fc1.bias.data, -spread, spread)
+
+        spread = 1 / np.sqrt(self.fc2.weight.data.size()[0])
+        torch.nn.init.uniform_(self.fc2.weight.data, -spread, spread)
+        torch.nn.init.uniform_(self.fc2.bias.data, -spread, spread)
+
+        spread = self.config.last_layer_init
+        torch.nn.init.uniform_(self.fc_to_Q.weight.data, -spread, spread)
+        torch.nn.init.uniform_(self.fc_to_Q.bias.data, -spread, spread)
 
     def forward(self, state, action):
 
-        state_features = F.relu(self.fc_from_state(state))
+        state_features = self.bn1(self.fc1(state))
+        state_features = F.relu(state_features)
+
         state_features_actions =  torch.cat((state_features, action), dim=1)
-        state_action_features = F.relu(self.fc_state_action(state_features_actions))
+        state_action_features =  self.bn2(self.fc2(state_features_actions))
+        state_action_features = F.relu(state_action_features)
+        
         Q_hat = self.fc_to_Q(state_action_features)
 
         return Q_hat        
